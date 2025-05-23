e2954a475f7fe495ccbc3c2786bd9105c69803fe
Dmitry Sokolov <wotcool1@yandex.ru>
2025-05-23 15:38:49 +0300

    Выполнена логика HistoryManager

--- src/main/ru/yandex/practicum/model/Node.java
+++ src/main/ru/yandex/practicum/model/Node.java
@@ -1,11 +1,11 @@
 package main.ru.yandex.practicum.model;
 
-public class Node <T> {
+public class Node <T>{
     public T value;
     public Node<T> next;
     public Node<T> prev;
 
-    public Node(T value){
+    public Node(T value) {
      this.value = value;
      this.next = null;
      this.prev = null;
