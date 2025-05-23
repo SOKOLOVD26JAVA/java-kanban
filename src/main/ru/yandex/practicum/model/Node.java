43c062129f41fd44b500e3ddf8b006a54c0d0f20
Dmitry Sokolov <wotcool1@yandex.ru>
2025-05-23 15:46:09 +0300

    ..

--- src/main/ru/yandex/practicum/model/Node.java
+++ src/main/ru/yandex/practicum/model/Node.java
@@ -1,9 +1,9 @@
 package main.ru.yandex.practicum.model;
 
-public class Node < T >{
+public class Node <T>{
     public T value;
-    public Node < T > next;
-    public Node < T > prev;
+    public Node <T> next;
+    public Node <T> prev;
 
     public Node(T value) {
      this.value = value;
